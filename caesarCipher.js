7cf168e0c43bda0ac882a37546b7d33da632f129
snowjelly <broadwellconnor@gmail.com>
2024-01-23 14:57:50 -0600

    support for spaces and capitals

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,3 +1,37 @@
-function caesarCipher(str, shift) {}
+function caesarCipher(str, shift) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let strCrypt = "";
+  let cryptLetter = "";
+
+  function getCryptLetter(decryptLetter) {
+    if (decryptLetter === decryptLetter.toUpperCase()) {
+      cryptLetter = alphabet.indexOf(decryptLetter.toLowerCase()) + shift;
+      if (cryptLetter > 25) {
+        cryptLetter -= 26;
+      }
+      strCrypt += alphabet
+        .substring(cryptLetter, cryptLetter + 1)
+        .toUpperCase();
+    } else {
+      cryptLetter = alphabet.indexOf(decryptLetter) + shift;
+
+      if (cryptLetter > 25) {
+        cryptLetter -= 26;
+      }
+      strCrypt += alphabet.substring(cryptLetter, cryptLetter + 1);
+    }
+  }
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (str.charAt(i) === " ") {
+      strCrypt += str.charAt(i);
+      continue;
+    }
+
+    getCryptLetter(str.charAt(i));
+  }
+
+  return strCrypt;
+}
 
 export default caesarCipher;
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -3,3 +3,11 @@ import caesarCipher from "./caesarCipher";
 test('string "sol badguy" becomes "tpm cbehvz"', () => {
   expect(caesarCipher("sol badguy", 1)).toBe("tpm cbehvz");
 });
+
+test('string "zzz" becomes "bbb"', () => {
+  expect(caesarCipher("zzz", 2)).toBe("bbb");
+});
+
+test('string "Water Bottle" becomes "Aexiv Fsxxpi"', () => {
+  expect(caesarCipher("Water Bottle", 4)).toBe("Aexiv Fsxxpi");
+});
